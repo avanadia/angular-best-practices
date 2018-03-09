dd1f77887dc4d0fd37f5d6111dde64e767161798
anthony_vanadia <31926505+avanadia@users.noreply.github.com>
2018-03-08 21:18:20 -0500

    decored input property in loadingspinner

--- src/app/shared/loading-spinner.component.ts
+++ src/app/shared/loading-spinner.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'wb-loading-spinner',
   template: '<img *ngIf="loading" src="../../assets/images/loading.gif" />',
-  inputs: ['loading']
 })
+
 export class LoadingSpinnerComponent {
-  loading:boolean
+  @Input() loading:boolean
 }
