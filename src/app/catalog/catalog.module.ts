1ece922dd2cea53ebb62d4bfc36dcae50efc5e44
anthony_vanadia <31926505+avanadia@users.noreply.github.com>
2018-03-09 09:58:21 -0500

    added filterclasses.service

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Router } from '@angular/router';
 
 import { CatalogComponent } from './catalog.component';
 import { CatalogRepositoryService } from './catalog-repository.service';
+import { FilterClassesService } from './filter-classes.service';
 import { SharedModule } from '../shared/shared.module'
 
 @NgModule({
     imports: [ RouterModule, SharedModule ],
     exports: [],
     declarations: [ CatalogComponent ],
-    providers: [ CatalogRepositoryService ]
+    providers: [ CatalogRepositoryService, FilterClassesService ]
 })
 
 export class CatalogModule { }
